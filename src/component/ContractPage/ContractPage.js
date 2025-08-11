35d98b712f88c1e455b5680aaea22c2ee732bd9e
EUNCHAE <2snha@naver.com>
2025-08-11 20:34:00 +0900

    chore: css 수정

--- src/component/ContractPage/ContractPage.js
+++ src/component/ContractPage/ContractPage.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import styles from './ContractPage.module.css';
-import { FaRegFileAlt, FaUser } from 'react-icons/fa';
-import { MdCheckCircleOutline } from 'react-icons/md';
-import { BsQuestionCircle } from 'react-icons/bs';
 import { useNavigate } from 'react-router-dom';
+import { MdCheckCircleOutline, MdOutlineDocumentScanner } from "react-icons/md";
+import { FaRegFileAlt, FaUser } from 'react-icons/fa';
+import { BsQuestionCircle } from 'react-icons/bs';
+import { FiCheck } from "react-icons/fi";
 
 export default function ContractPage() {
     const navigate = useNavigate();
@@ -27,31 +28,33 @@ export default function ContractPage() {
                 role="button"
                 tabIndex={0}
             >
-                <span>계약서<br />가져오기</span>
-                <div className={styles.scanIcon}></div>
+                <span className={styles.mainCardTitle}>계약서<br/>가져오기</span>
+                <MdOutlineDocumentScanner className={styles.scanIcon} size={36} />
             </div>
 
             <div className={styles.subCards}>
-                <div className={styles.card}>
+                <div className={`${styles.card} ${styles.cardQA}`}>
                     <span className={styles.cardTitle}>AI 대화</span>
                     <p className={styles.cardDesc}>계약에 대해<br />궁금한게 있나요?</p>
-                    <BsQuestionCircle size={32} className={styles.cardIcon} />
+                    <span className={styles.cardQMark}>?</span>
                 </div>
 
-                <div className={styles.card}>
+                <div className={`${styles.card} ${styles.cardChecklist}`}>
                     <span className={styles.cardTitle}>체크리스트</span>
-                    <p className={styles.cardDesc}>8개<br />조회하기</p>
-                    <MdCheckCircleOutline size={32} className={styles.cardIcon} />
+                    <p className={styles.cardDesc}><strong>8개</strong><br />조회하기</p>
+                    <div className={styles.cardBadge}>
+                        <FiCheck className={styles.cardBadgeIcon} size={28} />
+                    </div>
                 </div>
             </div>
 
             <div className={styles.tabBar}>
                 <div className={styles.tabItemActive}>
-                    <FaRegFileAlt />
+                    <FaRegFileAlt size={25} />
                     <span>내 계약</span>
                 </div>
                 <div className={styles.tabItem}>
-                    <FaUser />
+                    <FaUser size={25} />
                     <span>프로필</span>
                 </div>
             </div>
